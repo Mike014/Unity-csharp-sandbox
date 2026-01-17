dbc2bc4a456f699d1e08eca5355e4e4e73915e86
Mike014 <mikgrimaldi7@gmail.com>
2026-01-17 11:01:47 +0100

    Added basic Enum concept

---
+++ Assets/Script/LearningCurve/LearningCurve7.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve7 : MonoBehaviour
+{
+
+    // Definiamo quali sono le opzioni possibili
+    enum PlayerAction
+    {
+        Attack,
+        Defend,
+        Flee
+    };
+
+    [SerializeField]
+    PlayerAction currentAction;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentAction = PlayerAction.Attack;
+            Debug.Log("Current Action : " + currentAction);
+        }
+        else if (Input.GetKey(KeyCode.Tab))
+        {
+            currentAction = PlayerAction.Defend;
+            Debug.Log("Current Action : " + currentAction);
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            currentAction = PlayerAction.Flee;
+            Debug.Log("Current Action : " + currentAction);
+        }
+        else
+        {
+            Debug.Log("No Player Action");
+        }
+    }
+}
